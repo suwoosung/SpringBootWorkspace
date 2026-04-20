e12ce11c1bb8177f9060a263c2298107b7571cde
DESKTOP-8R59NPD\user1 <gwj9870@gmail.com>
2026-04-20 17:39:25 +0900

    수업

---
+++ src/main/java/com/kh/menu/security/controller/AuthController.java
@@ -0,0 +1,188 @@
+package com.kh.menu.security.controller;
+
+import static com.kh.menu.security.utils.CookieUtil.ACCESS_COOKIE;
+import static com.kh.menu.security.utils.CookieUtil.REFERSH_COOKIE;
+import static com.kh.menu.security.utils.CookieUtil.ROLE_COOKIE;
+import static com.kh.menu.security.utils.CookieUtil.createTokenCookie;
+
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kh.menu.security.model.dto.AuthDto.AuthResult;
+import com.kh.menu.security.model.dto.AuthDto.LoginRequest;
+import com.kh.menu.security.model.dto.AuthDto.User;
+import com.kh.menu.security.model.provider.JWTProvider;
+import com.kh.menu.security.model.service.AuthService;
+import com.kh.menu.security.model.service.KaKaoService;
+import com.kh.menu.security.utils.CookieUtil;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+@Slf4j
+public class AuthController {
+	
+	private final AuthService service;
+	private final KaKaoService kakaoService;
+	private final JWTProvider jwt;
+	
+	@PostMapping("/login")
+	public ResponseEntity<AuthResult> login(@RequestBody LoginRequest req){
+		/* 
+		 * 로그인 
+		 *  - 현재 db에 존재하지 않는 이메일이면 404에러 반환
+		 *  - 프런트에서는 응답상태가 404인 경우 회원가입할지, 재로그인할지 처리
+		 *  - 이멜은 존재하나 비밀번호가 틀린경우 401상태(미인증)반환 => 재로그인
+		 *  - 모두 성공시 유저정보와 JWT토큰 반환
+		 * */
+		// 1) 사용자가 존재하는지 확인
+		boolean exists = service.existsByEmail(req.getEmail());
+		
+		if(!exists) {
+			// 2) 사용자가 존재하지 않는 경우 404상태 반환
+			return ResponseEntity.notFound().build();
+		}
+		// 3) 사용자가 존재한다면 인증처리하고, 인증정보 반환
+		try {
+			AuthResult result = service.login(req.getEmail(), req.getPassword());
+			
+			// 인증성공시 accessToken, refreshToken생성하여 클라이언트의 쿠키로 전달.
+			return makeResponse(result);
+		}catch(BadCredentialsException e) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+		}
+	}
+	
+	/*
+	 * 자동 회원가입
+	 *  */
+	@PostMapping("/signup")
+	public ResponseEntity<AuthResult> signup(
+			@RequestBody LoginRequest req){
+		
+		AuthResult result = service.signup(req);
+		return makeResponse(result);
+	}
+	
+	
+	
+	
+	private ResponseEntity<AuthResult> makeResponse(AuthResult result){
+		// AccessToken을 쿠키에 담아서 전달
+		ResponseCookie accessCookie = 
+				createTokenCookie(ACCESS_COOKIE, result.getAccessToken(),30);
+		ResponseCookie refreshCookie =
+				createTokenCookie(REFERSH_COOKIE, result.getRefreshToken(), 7);
+		
+		String roles = result.getUser().getRoles()
+						.stream().collect(Collectors.joining("|"));
+		ResponseCookie roleCookie 
+			= createTokenCookie(ROLE_COOKIE, roles, 30);
+		
+		return ResponseEntity
+				.ok()
+				.header(HttpHeaders.SET_COOKIE, refreshCookie.toString())
+				.header(HttpHeaders.SET_COOKIE, accessCookie.toString())
+				.header(HttpHeaders.SET_COOKIE, roleCookie.toString())
+				.body(result);
+	}
+	
+	@PostMapping("/logout")
+	public ResponseEntity<Void> logout(HttpServletRequest req){
+		// 클라이언트의 헤더에서 토큰 추출
+		String accessToken = CookieUtil.resolveAccessToken(req);
+		
+		if(accessToken != null) {
+			// accessToken에 값이 있다면 카카오서비스 로그아웃 요청
+			
+		}
+		
+		// 로그아웃처리(쿠키 만료처리)
+		ResponseCookie refresh = createTokenCookie
+				(REFERSH_COOKIE, "", 0);
+		ResponseCookie access = createTokenCookie
+				(ACCESS_COOKIE, "", 0);
+		ResponseCookie roles = createTokenCookie
+				(ROLE_COOKIE, "", 0);
+		return ResponseEntity
+				.noContent()
+				.header(HttpHeaders.SET_COOKIE, refresh.toString())
+				.header(HttpHeaders.SET_COOKIE, access.toString())
+				.header(HttpHeaders.SET_COOKIE, roles.toString())
+				.build();
+	}
+	
+	// accessToken재발급 url
+	@PostMapping("/refresh")
+	public ResponseEntity<AuthResult> refresh(
+			@CookieValue(name = REFERSH_COOKIE, required = false) 
+			String refreshCookie
+			){
+		if(refreshCookie == null || refreshCookie.isBlank()) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+		}
+		
+		// 쿠키가 있으면 쿠키를 검증하여 새로운 accessToken생성
+		AuthResult result = service.refreshByCookie(refreshCookie);
+		
+		// 새로 발급된 토큰을 쿠키에 담기
+		ResponseCookie accessCookie = 
+				createTokenCookie(ACCESS_COOKIE, result.getAccessToken(), 30);
+
+		String roles = result.getUser().getRoles().stream()
+				.collect(Collectors.joining("|"));
+		
+		ResponseCookie roleCookie = 
+				createTokenCookie(ROLE_COOKIE, roles, 30);
+		
+		return ResponseEntity.ok()
+				.header(HttpHeaders.SET_COOKIE, accessCookie.toString())
+				.header(HttpHeaders.SET_COOKIE, roleCookie.toString())
+				.body(result);
+	}
+	
+	
+	//사용자정보 요청용 api
+	@GetMapping("/me")
+	public ResponseEntity<User> getUserInfo(HttpServletRequest req){
+		// 요청헤더에서 토큰 추출
+		String accessToken = CookieUtil.resolveAccessToken(req);
+		if(accessToken == null) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+		}
+		
+		// id추출
+		Long userId = jwt.getUserId(accessToken, ACCESS_COOKIE);
+		
+		// 사용자정보 조회
+		User user = service.findUserByUserId(userId);
+		
+		if(user == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		return ResponseEntity.ok(user);
+	}
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/kh/menu/security/model/service/AuthService.java
@@ -0,0 +1,123 @@
+package com.kh.menu.security.model.service;
+
+import java.util.List;
+
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.kh.menu.security.model.dao.AuthDao;
+import com.kh.menu.security.model.dto.AuthDto.AuthResult;
+import com.kh.menu.security.model.dto.AuthDto.LoginRequest;
+import com.kh.menu.security.model.dto.AuthDto.User;
+import com.kh.menu.security.model.dto.AuthDto.UserAuthority;
+import com.kh.menu.security.model.dto.AuthDto.UserCredential;
+import com.kh.menu.security.model.provider.JWTProvider;
+import com.kh.menu.security.utils.CookieUtil;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AuthService {
+	
+	private final AuthDao authDao;
+	private final PasswordEncoder encoder; // bean객체 securityConfig에만들기
+	private final KaKaoService service;
+	private final JWTProvider jwt;
+	
+	
+	public boolean existsByEmail(String email) {
+		User user = authDao.findUserByEmail(email);
+		return user != null;
+	}
+
+
+	public AuthResult login(String email, String password) {
+		// 1. 사용자 정보 조회
+		User user = authDao.findUserByEmail(email);
+		
+		if(!encoder.matches(password, user.getPassword())) {
+			throw new BadCredentialsException("비밀번호가 일치하지 않습니다.");
+		}
+		
+		// 2) 토큰발급
+		String accessToken = jwt.createAccessToken(user.getId() , 30);
+		String refreshToken = jwt.createRefreshToken(user.getId(), 7);
+		
+		User userNoPassword = User
+								.builder()
+								.id(user.getId())
+								.email(user.getEmail())
+								.profile(user.getProfile())
+								.name(user.getName())
+								.roles(user.getRoles())
+								.build();
+		return AuthResult.builder()
+				.accessToken(accessToken)
+				.refreshToken(refreshToken)
+				.user(userNoPassword)
+				.build();
+	}
+
+	@Transactional
+	public AuthResult signup(LoginRequest req) {
+		String email = req.getEmail();
+		String password = req.getPassword();
+		
+		// 1) Users테이블에 데이터 추가
+		User user = User.builder()
+						.email(email)
+						.name(email.split("@")[0])
+						.build();	
+		authDao.insertUser(user);
+		
+		// 2) Credentail에 데이터 추가
+		UserCredential cred = UserCredential
+								.builder()
+								.userId(user.getId())
+								.password(encoder.encode(password))
+								.build();
+		authDao.insertCred(cred);
+		// 3) 권한추가
+		UserAuthority auth = UserAuthority
+								.builder()
+								.userId(user.getId())
+								.roles(List.of("ROLE_USER"))
+								.build();
+		authDao.insertUserRole(auth);		
+		
+		// 4) 회원가입 완료 후 , 토큰 발급
+		String accessToken = jwt.createAccessToken(user.getId() , 30);
+		String refreshToken = jwt.createRefreshToken(user.getId(), 7);
+		
+		user = authDao.findUserByUserId(user.getId()); // 비밀번호제외 유저정보
+		
+		return AuthResult.builder()
+				.accessToken(accessToken)
+				.refreshToken(refreshToken)
+				.user(user)
+				.build();
+	}
+
+
+	public AuthResult refreshByCookie(String refreshCookie) {
+		Long userId = jwt.getUserId(refreshCookie, CookieUtil.REFERSH_COOKIE);
+		User user = authDao.findUserByUserId(userId);
+		
+		String accessToken = jwt.createAccessToken(userId, 30);
+		
+		return AuthResult.builder()
+				.accessToken(accessToken)
+				.user(user)
+				.build();
+	}
+
+	public User findUserByUserId(Long userId) {
+		return authDao.findUserByUserId(userId);
+	}
+
+}
